4904a01252bf7f4fc22e06b36fa1d2df8a15a33e
nicoleo97 <73941175+nicoleo97@users.noreply.github.com>
2025-07-13 09:01:56 +0000

    draft01 complete

--- lotus.py
+++ lotus.py
@@ -6,55 +6,41 @@ monate = [
     'Juli','August','September','Oktober','November','Dezember'
 ]
 
-# Titel und Farben
-titles = [
-    ("Fantasie, Kreativität und\nkünstlerisches Gestalten", "#C00798"),
-    ("Musik", "#932ECE"),
-    ("Motorik", "#47E3FF"),
-    ("Sprache, Kommunikation\nund Medien", "#93FF2F"),
-    ("MONAT", "#000000"),
-    ("Körper, Umwelt und\nGesundheit", "#239B89"),
-    ("Soziales und Gesundheit", "#FF8800"),
-    ("Wahrnehmung, Emotion und\nKognition", "#198628"),
-    ("Mathematik,\nNaturwissenschaft und\nTechnik", "#D9E900")
+# Pre-Set Überschriften
+titel = [
+"Fantasie, Kreativität und\nkünstlerisches Gestalten",
+"Musik",
+"Motorik",
+"Sprache, Kommunikation\nund Medien",
+"",
+"Körper, Umwelt und\nGesundheit",
+"Soziales und Gesundheit",
+"Wahrnehmung, Emotion und\nKognition",
+"Mathematik,\nNaturwissenschaft und\nTechnik"
 ]
 
+# Pre-Set Farben
+farben = [
+"#C00798",
+"#932ECE",
+"#47E3FF",
+"#93FF2F",
+"",
+"#239B89",
+"#FF8800",
+"#198628",
+"#D9E900"
+]
 # Funktion zur Umrechnung Hex → RGB (für fpdf)
 def hex_to_rgb(hex_color):
     hex_color = hex_color.lstrip('#')
     return tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
 
 # PDF erzeugen
-def create_grid_pdf(monat):
+def create_grid_pdf(monat, titel, farben):
     pdf = FPDF(orientation='L', unit= 'mm', format= 'A4')
     pdf.add_page() # erzeugt überhaupt erste Seite
     pdf.set_auto_page_break(False) # keine automatischen neuen Seiten
-
-    # Pre-Set Überschriften
-    titel = [
-    "Fantasie, Kreativität und\nkünstlerisches Gestalten",
-    "Musik",
-    "Motorik",
-    "Sprache, Kommunikation\nund Medien",
-    "MONAT",
-    "Körper, Umwelt und\nGesundheit",
-    "Soziales und Gesundheit",
-    "Wahrnehmung, Emotion und\nKognition",
-    "Mathematik,\nNaturwissenschaft und\nTechnik"
-    ]
-
-    # Pre-Set Farben
-    farben = [
-    "#C00798",
-    "#932ECE",
-    "#47E3FF",
-    "#93FF2F",
-    "#000000",
-    "#239B89",
-    "#FF8800",
-    "#198628",
-    "#D9E900"
-    ]
 
     box_width = 91 # mm
     box_height = 62 # mm
@@ -75,6 +61,7 @@ def create_grid_pdf(monat):
             # Box zeichnen
             pdf.rect(x,y,box_width, box_height)
 
+            # Überschriften und Monat schreiben
             if indx == 4:
                 pdf.set_xy(x+2, y + (box_height-8)/2)
                 pdf.set_font('Helvetica', size=40, style='B')
@@ -86,6 +73,16 @@ def create_grid_pdf(monat):
                 r, g, b = hex_to_rgb(farben[indx])
                 pdf.set_text_color(r, g, b)
                 pdf.multi_cell(w= box_width-4, h=8, txt=titel[indx], align= 'C')
+            
+            # Inhalt / Text einfügen
+            if indx == 4:
+                pass
+            else:
+                pdf.set_xy(x+2, y+25) # 2mm Padding
+                pdf.set_font('Helvetica', size=14)
+                r, g, b = hex_to_rgb(farben[indx])
+                pdf.set_text_color(r, g, b)
+                pdf.multi_cell(w= box_width-4, h=8, txt=input_data[indx], align= 'C')
 
     return pdf.output(dest='S').encode('latin1')
 
@@ -95,6 +92,13 @@ st.title('Lotus Generator')
 st.subheader('Monat eingeben')
 monat = st.selectbox('Monat auswählen', monate)
 
+input_data=[]
+for i in range(len(titel)):
+    if i ==4:
+        pass
+    input = st.text_area(titel[i])
+    input_data.append(input)
+
 if st.button("PDF generieren"):
-    pdf_bytes = create_grid_pdf(monat)
+    pdf_bytes = create_grid_pdf(monat,titel,farben)
     st.download_button("Download PDF", pdf_bytes, file_name="raster.pdf", mime="application/pdf")
